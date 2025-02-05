e8fed44e0c021c2ee06e95ab51a5029e5ca2ee13
AndreSouza94 <andre_souzaprof@hotmail.com>
2025-02-05 00:37:44 -0300

    Add files via upload

--- Entregador.java
+++ Entregador.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,12 +12,21 @@
  */
 //André Faria de Souza  RA: 2101106
 public class Entregador extends Funcionario{
-
+    private String veiculo;
+    private String tipoHabilit; 
+    
     static boolean isSelected() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    private String veiculo;
-    private String tipoHabilit;
+    
+    // 🔹 Construtor vazio (necessário para alguns frameworks ou inicializações dinâmicas)
+    public Entregador() {
+        super(); // Chama o construtor vazio da superclasse (se existir)
+        this.veiculo = "";
+        this.tipoHabilit = "";
+    }
+
+    
     public Entregador(String nome, String telefone, String veiculo, String tipoHabilit){
         super(nome,telefone);
         this.veiculo = veiculo;
@@ -38,10 +50,15 @@ public class Entregador extends Funcionario{
     }
     
      // Método para exibir dados
-    public void exibirDados() {
-        System.out.println("Nome: " + getNome()); // Supondo que getNome() esteja na classe Funcionario
-        System.out.println("Telefone: " + getTelefone()); // Supondo que getTelefone() esteja na classe Funcionario
-        System.out.println("Veiculo: " + veiculo);
-        System.out.println("Habilitacao: " + tipoHabilit);
-    }
+       public void exibirDados() {
+             JOptionPane.showMessageDialog(null,
+        "Nome: " + getNome() + "\n" +
+        "Telefone: " + getTelefone() + "\n" +
+        "Veículo: " + veiculo + "\n" +
+        "Habilitação: " + tipoHabilit,
+        "Dados do Entregador",
+        JOptionPane.INFORMATION_MESSAGE
+    );
 }
+}
+
